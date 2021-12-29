41e5fad94d27a253761abed6fc25a44774abb8f0
Abdullah Yusefi <a.yusefi1991@gmail.com>
2021-12-29 15:46:19 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+int main(){
+    std::cout << "hello world!" << std::endl;
+    cv::Mat image = cv::imread("lenna.jpeg", cv::IMREAD_GRAYSCALE);
+    if(image.empty()){
+        std::cerr << "File not found." << std::endl;
+        return 1;
+    }
+
+    cv::namedWindow("window");
+    cv::imshow("window", image);
+    cv::waitKey(0);
+    return 0;
+}
